eb5e297fbec23aaeb5913a9d07deab3c56fb77c4
dhkimcoding <dhkimcoding@gmail.com>
2025-02-15 11:47:46 +0900

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof(a) !== 'number' || Math.round(a) !== a) {
+        return "ERROR";
+    }
+    if (typeof(b) !== 'number' || Math.round(b) !== b) {
+        return "ERROR";
+    }
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    if (a > b) {
+        let tmp;
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 

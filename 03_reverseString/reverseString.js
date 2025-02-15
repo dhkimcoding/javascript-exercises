565bce53a2b742802b2ee4f2d22a0fd4d12f9e89
dhkimcoding <dhkimcoding@gmail.com>
2025-02-15 11:01:54 +0900

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const list = str.split("");
+    const reversed = list.reverse();
+    const res = reversed.join("");
+    return res;
 };
 
 // Do not edit below this line

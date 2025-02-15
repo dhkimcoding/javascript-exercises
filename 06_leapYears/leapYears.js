92aa6cc34a6334770e8a3676258a2f8733b11b78
dhkimcoding <dhkimcoding@gmail.com>
2025-02-15 11:51:41 +0900

    Complete 06_learYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let res;
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                res = true;
+            } else {
+                res = false;
+            }
+        } else {
+            res = true;
+        }
+    } else {
+        res = false;
+    }
+    return res;
 };
 
 // Do not edit below this line

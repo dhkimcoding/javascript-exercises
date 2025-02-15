3a6efdfc2817945f56b16ad5e86bee69a415aba2
dhkimcoding <dhkimcoding@gmail.com>
2025-02-15 12:13:19 +0900

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  let res = (deg - 32) * 5 / 9;
+  let rounded = Math.round(res * 10) / 10;
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  let res = (deg * 9 / 5 + 32);
+  let rounded = Math.round(res * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line

ab87b4f7270e2309edad9406043bee65f0b6caf1
dhkimcoding <dhkimcoding@gmail.com>
2025-02-15 11:40:16 +0900

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(list1, ...list2) {
+    const deleteIndex = [];
+    for (let i = 0; i < list2.length; i++) {
+        for (let j = 0; j < list1.length; j++) {
+            if (list2[i] === list1[j]) {
+                deleteIndex.push(j);
+            }
+        }
+    }
+    const res = [];
+    for (let i = 0; i < list1.length; i++) {
+        if (deleteIndex.includes(i)) continue;
+        res.push(list1[i]);
+    }
+    return res;
 };
 
 // Do not edit below this line

af93c96dd885a99a367411070896856d11614c7c
dhkimcoding <dhkimcoding@gmail.com>
2025-02-17 17:01:13 +0900

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const allLetter = str.toLowerCase().split("");
+    const onlyAlpha = allLetter.filter((ch) => (ch >= "a" && ch <= "z") || (ch >= "0" && ch <= "9"));
+    let ok = true;
+    for (let i = 0; i < onlyAlpha.length / 2; i++) {
+        if (onlyAlpha[i] !== onlyAlpha[onlyAlpha.length-1-i]) {
+            ok = false;
+            break;
+        }
+    }
+    return ok;
 };
 
 // Do not edit below this line

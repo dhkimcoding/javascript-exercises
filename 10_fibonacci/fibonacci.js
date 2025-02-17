4e31a3d665c1b982ccc8b18174abe678683b62f0
dhkimcoding <dhkimcoding@gmail.com>
2025-02-17 17:13:53 +0900

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if (num === 0) return 0;
+    if (num < 0) return "OOPS";
+    if (num === 1 || num === 2) return 1;
+    const fibo = [1, 1];
+    let idx = 0;
+    while (idx <= num-1) {
+        fibo.push(fibo[idx] + fibo[idx+1]);
+        idx++;
+    }
+    return fibo[num-1];
 };
 
 // Do not edit below this line

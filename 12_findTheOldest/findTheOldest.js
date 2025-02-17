47299fd5deefa313b79eda68a024458bc73e941f
dhkimcoding <dhkimcoding@gmail.com>
2025-02-17 17:24:42 +0900

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) =>{
+        let aAge;
+        let bAge;
+        if (!a.yearOfDeath) {
+            aAge = (new Date()).getFullYear() - a.yearOfBirth;
+        } else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
 
+        if (!b.yearOfDeath) {
+            bAge = (new Date()).getFullYear() - b.yearOfBirth;
+        } else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        
+        return aAge - bAge;
+    });
+    return people[people.length-1];
 };
 
 // Do not edit below this line
